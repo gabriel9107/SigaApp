43154bd1d1e3ba7a8226304cc85d026558568ab9
gabriel9107 <gabriel9107@gmail.com>
2022-11-07 16:00:37 -0400

    provider

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/provider/login_from_prodivder.dart';
 import 'package:flutter_application_2/widgets/widgets.dart';
 import 'package:flutter_application_2/ui/input_decorations.dart';
+
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -21,7 +24,11 @@ class LoginScreen extends StatelessWidget {
             SizedBox(height: 10),
             Text('Login', style: Theme.of(context).textTheme.headline4),
             SizedBox(height: 30),
-            _LoginForm()
+            ChangeNotifierProvider(
+              create: (_) => LoginFormProvider(),
+              child: _LoginForm(),
+            )
+            // _LoginForm()
           ],
         )),
         SizedBox(height: 50),
@@ -42,42 +49,50 @@ class LoginScreen extends StatelessWidget {
 class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final loginForm = promiseToFuture(jsPromise)
     return Container(
       child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hinText: '40221025725',
-                labelText: 'Nombre de usuario',
-                prefixIcon: Icons.alternate_email_sharp),
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hinText: '********',
-                labelText: 'Contraseña',
-                prefixIcon: Icons.lock_outline),
-          ),
-          SizedBox(height: 50),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              disabledColor: Colors.grey,
-              elevation: 0,
-              color: Colors.deepPurple,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: Text('Ingresar', style: TextStyle(color: Colors.white)),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hinText: '40221025725',
+                    labelText: 'Nombre de usuario',
+                    prefixIcon: Icons.alternate_email_sharp),
               ),
-              onPressed: () {}),
-        ],
-      )),
+              SizedBox(height: 30),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hinText: '********',
+                    labelText: 'Contraseña',
+                    prefixIcon: Icons.lock_outline),
+                validator: (value) {
+                  return (value != null && value.length >= 6)
+                      ? null
+                      : 'La contraseña debe de ser de 6 caracteres';
+                },
+              ),
+              SizedBox(height: 50),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  disabledColor: Colors.grey,
+                  elevation: 0,
+                  color: Colors.deepPurple,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child:
+                        Text('Ingresar', style: TextStyle(color: Colors.white)),
+                  ),
+                  onPressed: () {}),
+            ],
+          )),
     );
   }
 }
